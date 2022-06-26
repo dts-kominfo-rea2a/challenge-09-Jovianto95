fadc12aa6776fdfd96dc91833d4440203e5f3ed3
Jovianto Trisila <mandevil41@gmail.com>
2022-06-26 22:39:24 +0700

    Done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,21 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import Contact from './Contact.css';
 
-const Contact = () => {
+const Contact = (propsData) => {
+    const item = propsData.data;
     return (
-        <></>
+        <div className='container'>
+            <div className='image'>
+                <img src={item.photo} className='img' alt={item.photo}></img>
+            </div>
+            <div className='content'>
+                <b><p key={item.name}>{item.name}</p></b>
+                <p key={item.phone}>{item.phone}</p>
+                <p key={item.email}>{item.email}</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import Header from './Header.css';
 
 const Header = () => {
     return (
-        <></>
+        <div style={{textAlign: 'center'}}>
+            <h2>Call a Friend</h2>
+            <p>Your Friendly Contact App</p>
+            <div style = {{textAlign: 'center'}}><hr /></div>
+        </div>
     )
 }
 
